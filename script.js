239ff9650a492a0daccbfe29d0b31bf9e57525a5
Peter Nguyen <peternguyenforwork@gmail.com>
2023-11-16 10:50:30 -0800

    adjusted the sendNote function

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 function getRandomColor() {}
 
 function sendNote() {
+  const inputElement = document.getElementById("input");
+  const noteContent = inputElement.value;
+  inputElement.value = "";
+
   let newNote = document.createElement("li");
-  newNote.innerHTML = "Hi";
+  newNote.innerHTML = noteContent;
   let allNotes = document.getElementById("notes");
   allNotes.appendChild(newNote);
 }
