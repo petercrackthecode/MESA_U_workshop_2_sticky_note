761c5cdc0f34b3d847bf1231cf9c20e2f1ac902b
Peter Nguyen <peternguyenforwork@gmail.com>
2023-11-16 12:08:08 -0800

    completed the basic functionality

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-function getRandomColor() {}
+function getRandomColor() {
+  const COLORS = [
+    "#a2d1fc",
+    "#ffc2cd",
+    "#97aedc",
+    "#d967ae",
+    "#b7cf8e",
+    "#d5edc2",
+  ];
+
+  return COLORS[Math.floor(Math.random() * COLORS.length)];
+}
 
 function sendNote() {
   const inputElement = document.getElementById("input");
@@ -6,6 +17,9 @@ function sendNote() {
   inputElement.value = "";
 
   let newNote = document.createElement("li");
+  newNote.style = "height: 150px;";
+  newNote.style.backgroundColor = getRandomColor();
+  newNote.classList.add("container");
   newNote.innerHTML = noteContent;
   let allNotes = document.getElementById("notes");
   allNotes.appendChild(newNote);
