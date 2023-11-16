0d268f7b24ae998cad6098a94fe029d08dda0e8c
VinnyXP <viph0009@gmail.com>
2023-11-16 11:27:22 -0800

    Add note, render notes, some css

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 function getRandomColor() {}
 
-function sendNote() {
+let notes = []
+
+/*function createNote() {
   const inputElement = document.getElementById("input");
   const noteContent = inputElement.value;
   inputElement.value = "";
@@ -9,4 +11,36 @@ function sendNote() {
   newNote.innerHTML = noteContent;
   let allNotes = document.getElementById("notes");
   allNotes.appendChild(newNote);
+} */
+
+function createNote() {
+  const inputElement = document.getElementById("input");
+  const noteContent = inputElement.value;
+  inputElement.value = "";
+
+  if (noteContent !== '') {
+    const newNote = {
+      id: notes.length + 1,
+      content: noteContent
+    };
+
+    notes.push(newNote);
+    /*saveNotesToStorage(); */
+    renderNotes();
+  }
+}
+
+function renderNotes() {
+  const notesContainer = document.getElementById('notesContainer');
+  notesContainer.innerHTML = '';
+
+  notes.forEach(note => {
+    const noteElement = document.createElement('div');
+    noteElement.classList.add('note');
+    noteElement.innerHTML = `
+      <span contenteditable="true" class="editable" oninput="updateNoteContent(${note.id}, this.innerText)">${note.content}</span>
+      <button onclick="deleteNote(${note.id})" class="delete-btn">X</button>
+    `;
+    notesContainer.appendChild(noteElement);
+  });
 }
